801d720b8eea5b88bce84e95e637da49533015a7
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-06-16 19:00:50 -0300

    Interface para cálculo do reajuste de salário

---
+++ src/DesignPattern/Strategy/Solucao/CalculadorReajusteAnualSalario.java
@@ -0,0 +1,7 @@
+package DesignPattern.Strategy.Solucao;
+
+import DesignPattern.Strategy.Funcionario;
+
+public interface CalculadorReajusteAnualSalario {
+    void calculaReajusteAnual(Funcionario funcionario);
+}

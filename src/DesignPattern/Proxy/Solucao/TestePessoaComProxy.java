b8e2f0eaa39e232d6caf39ec60a8313d9c468747
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-06-16 15:08:10 -0300

    Teste de Service, Repository e Proxy

---
+++ src/DesignPattern/Proxy/Solucao/TestePessoaComProxy.java
@@ -0,0 +1,27 @@
+package DesignPattern.Proxy.Solucao;
+
+import java.time.LocalDate;
+import DesignPattern.Builder.Pessoa;
+import DesignPattern.Proxy.PessoaService;
+
+public class TestePessoaComProxy {
+    public static void main(String[] args) {
+        PessoaRepositoryProxy pessoaRepositoryProxy = new PessoaRepositoryProxy();
+        PessoaService pessoaService = new PessoaService(pessoaRepositoryProxy);
+
+        Pessoa pessoa = new Pessoa.PessoaBuilder()
+        .sobrenome("Alexandre")
+        .nome("Klinger")
+        .apelido("Kiko")
+        .documento("99999999999")
+        .dataNascimento(LocalDate.of(1992, 04, 21))
+        .build();
+
+        pessoaService.save(pessoa);
+
+        Pessoa pessoaRetornada = pessoaService.findById(1L);
+        System.out.println(pessoaRetornada);
+    }
+}
+    
+

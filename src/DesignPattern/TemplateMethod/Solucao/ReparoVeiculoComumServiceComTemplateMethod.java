8248245cd3e358011b1731746a14351287152230
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-06-16 23:27:43 -0300

    Implementa classe abstrata para veículo comum

---
+++ src/DesignPattern/TemplateMethod/Solucao/ReparoVeiculoComumServiceComTemplateMethod.java
@@ -0,0 +1,22 @@
+package DesignPattern.TemplateMethod.Solucao;
+
+import DesignPattern.TemplateMethod.VeiculoParaReparo;
+
+public class ReparoVeiculoComumServiceComTemplateMethod extends ReparoVeiculoService {
+    private VeiculoParaReparo veiculoParaReparo;
+
+    public ReparoVeiculoComumServiceComTemplateMethod(VeiculoParaReparo veiculoParaReparo) {
+        this.veiculoParaReparo = veiculoParaReparo;
+    }
+
+    // método definido como protected, então dá pra sobrescrever
+    @Override
+    protected void entradaOficina() {
+        System.out.println("Entrando na oficina comum...");
+    }
+
+    @Override
+    protected boolean veiculoParaReparo() {
+        return veiculoParaReparo.getPorcentagemDano() <= 70;
+    }
+}
